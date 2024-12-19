a4ef68c979f41c5d44cb9698dd58728417f643ca
Arhamsmit <arhamsmit@gmail.com>
2024-12-19 20:05:48 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// chapter 1
+// Question 1
+
+//    alert("Hello!");
+
+//chapter 2
+// Question 2
+
+//    var string = "Hello, World!";
+//     document.write(string);
+
+// chapter 3
+//  Question 
+
+// var number = 5;
+// document.write(number);
