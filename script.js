e9bbb5bd6328e1d00f90d0d1eb7c0cdd61f68c52
Arhamsmit <arhamsmit@gmail.com>
2024-12-26 22:33:32 +0500

    newcode

--- script.js
+++ script.js
@@ -14,3 +14,43 @@
 
 // var number = 5;
 // document.write(number);
+
+// Chapter 4 
+// Qeustion 1
+//legal variables
+
+// var my_Name; 
+// var myAgem;
+// var myAssignment2;
+// var $my;
+// var myvariable;
+
+// ilegal variable
+
+// var 1assignment;
+// var my-Name;
+// var my name;
+// var var;
+// var myvariable@
+
+// Chapter 5
+// Question 1
+// Math Expressions Familar operater
+
+// var num = 10;
+// var num2 = 2;
+// console.log(num + num2);
+// console.log(num - num2);
+// console.log(num * num2);
+// console.log(num / num2);
+
+// Chapter 6
+// Question 1
+// Math Expressions Unfamiliar operator
+
+// var a = 10;
+// var b = 2;
+// console.log(a++); // postincrement
+// console.log(++a); // preincrement
+// console.log(b--); // postdecrement
+// console.log(--b); // predecrement
